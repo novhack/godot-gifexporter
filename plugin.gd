ba0f832b3d208678a2c0b8e935f7b365e9b49739
novhack <42614907+novhack@users.noreply.github.com>
2020-05-17 01:59:15 +0200

    Properly create blank icon to not display error in Godot Editor.

--- plugin.gd
+++ plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("GifExporter", "Node", preload("res://addons/godot-gifexporter/src/GifExporter.gd"), Texture.new())
+	add_custom_type("GifExporter", "Node", preload("res://addons/godot-gifexporter/src/GifExporter.gd"), ImageTexture.new())
 
 func _exit_tree():
 	remove_custom_type("GifExporter")
